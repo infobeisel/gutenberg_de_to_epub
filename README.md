d7fcd234700f19c5215baa6ade3ec6c3dd20bd91
Philipp Beisel <infobeisel@gmail.com>
2025-06-19 12:12:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Ein linux bash script, um einen heruntergeladenen Text im html-Format von der Seite https://www.projekt-gutenberg.org/ in ein ebook im .epub-Format umzuwandeln.
+Calibre muss auf dem System installiert sein. Das script überspringt alle Verweise auf andere Werke und führt dann Calibre's "ebook-convert" aus.
+Anleitung:
+Platziere das script "gutenberg_de_to_epub.sh" neben die index.html-Datei und führe es aus.
---
+++ gutenberg_de_to_epub.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#name of the ebook: author name _ title
+COMBINED_NAME="$(basename "$(dirname "$PWD")")_$(basename "$PWD")"
+
+#option to rm everything except X
+extglob_was_enabled=$(shopt -p extglob)
+shopt -s extglob
+
+#make a directory "conversionTemp", copy everything
+rm -rf conversionTemp
+mkdir conversionTemp
+cp -r !(conversionTemp) conversionTemp
+
+
+#replace certain html sections from the files, as they lead to including the whole book library
+for file in *.html; do
+    if [[ "$file" != "index.html" && -f "$file" ]]; then
+        sed -i '/<div class="navi-gb">/,/weiter&nbsp;&gt;&gt;<\/a>&nbsp;<\/hr>/d' $file
+        sed -i '/<hr size="1" color="#808080">&nbsp/,/<\/div>/d' $file
+    elif [[ "$file" == "index.html" && -f "$file"  ]]; then
+        echo filename:
+        echo $file
+        sed -i '/<div class="navi-gb">/,/<\/div>/d' $file
+        sed -i '/^<p><h5><a href=/d' $file
+        #sed -i '/<p><h5><a href="..\/..\/autoren\/namen\//,/<\/a><\/h5>/d' $file
+        sed -i '/<div class="bottomnavi-gb">/,/<\/div>/d' $file
+    fi
+done
+
+#convert to ebook
+ebook-convert index.html "$COMBINED_NAME.epub"
+
+#restore original files from temp
+mv *.epub conversionTemp
+rm -rf !(conversionTemp)
+mv conversionTemp/* .
+rm -rf conversionTemp
+
+
+# Restore previous extglob state
+eval "$extglob_was_enabled"
